f82a06d7dd278bf65501c4cd31e84d09e2cdf24b
xujustinj <xu.justin.j@gmail.com>
2018-12-25 13:54:28 -0500

    Remove redundancies
    Removes:
    - unused resources in index.html
    - hexagonal_2048.js (contains no unique code)
    - uncalled functions from paint.js, tile.js
    - irrelevant comments
    
    Replaces some instances of bulky code with built-in p5 methods.

--- paint.js
+++ paint.js
@@ -1,75 +1,21 @@
-function paint_rectangle(name, x1, y1, x2, y2, red, green, blue, roundness) {
-  colorMode(RGB, 255);
-  noStroke();
-  fill(red, green, blue);
+function paint_regular_hexagon (x, y, s, colour) {
+    // Paints a regular hexagon that looks like this <=> on the canvas.
+    // The centre of the hexagon is located at (x,y).
+    // The distance between the centre of the hexagon and any of its vertices is
+    //   s, which is also its side length.
+    // The hexagon is filled with the consumed colour.
 
-  var left = x1;
-  var right = x2;
-  if (x1 > x2) {
-    left = x2;
-    right = x1;
-  }
+    fill(colour);
 
-  var top = y1;
-  var bottom = y2;
-  if (y1 > y2) {
-    top = y2;
-    bottom = y1;
-  }
+    const h = s * sin60;
+    const w = s * cos60;
 
-  rect(left, top, right - left, bottom - top, roundness);
-
-//  register("rectangle", name, (x1 + x2) / 2, (y1 + y2) / 2, left, right, top, bottom, red, green, blue, null, roundness);
-}
-
-function paint_regular_hexagon_unrounded(name, x, y, r, red, green, blue) { // regular hexagon that looks like this <=>
-  colorMode(RGB, 255);
-  stroke(red, green, blue);
-  fill(red, green, blue);
-
-  rect(x - (r - 1) / 2, y - (r - 1) * Math.sqrt(3) / 2, r - 1, (r - 1) * Math.sqrt(3)); // r - 1 serves the purpose of making everything smaller so that the strokes overlap in the centre of the object without leaving any ugly blank lines
-  triangle(x - (r - 1) / 2, y - (r - 1) * Math.sqrt(3) / 2, x - (r - 1), y, x - (r - 1) / 2, y + (r - 1) * Math.sqrt(3) / 2);
-  triangle(x + (r - 1) / 2, y - (r - 1) * Math.sqrt(3) / 2, x + (r - 1), y, x + (r - 1) / 2, y + (r - 1) * Math.sqrt(3) / 2);
-
-//  register("regular_hexagon", name, x, y, x - Math.abs(r), x + Math.abs(r), y - Math.abs(r) * Math.sqrt(3) / 2, red, green, blue, Math.abs(r), 0);
-}
-
-function register(type, name, x, y, left, right, top, bottom, r, g, b, p1, roundness) {
-  var exists;
-
-  for (var i = 0; i < shapes.length; i ++) {
-    if (shapes[i].name == name) {
-      shapes[i].type = type;
-      shapes[i].x = x;
-      shapes[i].y = y;
-      shapes[i].left = left;
-      shapes[i].right = right;
-      shapes[i].top = top;
-      shapes[i].bottom = bottom;
-      shapes[i].r = red;
-      shapes[i].g = green;
-      shapes[i].b = blue;
-      shapes[i].p1 = p1;
-      shapes[i].roundness = roundness;
-
-      exists = true;
-    }
-  }
-
-  if (!exists) {
-    shapes.unshift(new Object());
-    shapes[i].type = type;
-    shapes[i].name = name;
-    shapes[i].x = x;
-    shapes[i].y = y;
-    shapes[i].left = left;
-    shapes[i].right = right;
-    shapes[i].top = top;
-    shapes[i].bottom = bottom;
-    shapes[i].r = red;
-    shapes[i].g = green;
-    shapes[i].b = blue;
-    shapes[i].p1 = r;
-    shapes[i].roundness = 0;
-  }
+    beginShape();
+    vertex(x - s, y);
+    vertex(x - w, y - h);
+    vertex(x + w, y - h);
+    vertex(x + s, y);
+    vertex(x + w, y + h);
+    vertex(x - w, y + h);
+    endShape(CLOSE);
 }
--- tile.js
+++ tile.js
@@ -1,4 +1,4 @@
-var space = 12;
+const space = 12;
 
 function Tile() {
 
@@ -6,44 +6,42 @@ function Tile() {
   this.sideLength = 64;
 
   this.value = 0;
+  this.colour = colours[0];
 
-  this.shouldX = this.x = centreX;
-  this.shouldY = this.y = centreY;
-
-  this.red = 205;
-  this.green = 193;
-  this.blue = 180;
+  // This default behaviour is poor communication and should be put into the define function instead.
+  this.shouldX = this.x = centre;
+  this.shouldY = this.y = centre;
 
   this.define = function(n) {
     this.id = n;
     this.sideLength = 64;
 
     if ((n == 12) || (n == 13) || (n == 14)) {
-      this.shouldX = this.x -= this.sideLength * 3 + space * h * 2;
+      this.shouldX = this.x -= this.sideLength * 3 + space * sin60 * 2;
     } else if ((n == 3) || (n == 4) || (n == 11) || (n == 15)) {
-      this.shouldX = this.x -= this.sideLength * 1.5 + space * h;
+      this.shouldX = this.x -= this.sideLength * 1.5 + space * sin60;
     } else if ((n == 1) || (n == 6) || (n == 9) || (n == 17)) {
-      this.shouldX = this.x += this.sideLength * 1.5 + space * h;
+      this.shouldX = this.x += this.sideLength * 1.5 + space * sin60;
     } else if ((n == 7) || (n == 8) || (n == 18)) {
-      this.shouldX = this.x += this.sideLength * 3 + space * h * 2;
+      this.shouldX = this.x += this.sideLength * 3 + space * sin60 * 2;
     }
 
     if (n == 10) {
-      this.shouldY = this.y -= this.sideLength * h * 4 + space * 2;
+      this.shouldY = this.y -= this.sideLength * sin60 * 4 + space * 2;
     } else if ((n == 9) || (n == 11)) {
-      this.shouldY = this.y -= this.sideLength * h * 3 + space * 1.5;
+      this.shouldY = this.y -= this.sideLength * sin60 * 3 + space * 1.5;
     } else if ((n == 2) || (n == 8) || (n == 12)) {
-      this.shouldY = this.y -= this.sideLength * h * 2 + space;
+      this.shouldY = this.y -= this.sideLength * sin60 * 2 + space;
     } else if ((n == 1) || (n == 3)) {
-      this.shouldY = this.y -= this.sideLength * h + space * 0.5;
+      this.shouldY = this.y -= this.sideLength * sin60 + space * 0.5;
     } else if ((n == 4) || (n == 6)) {
-      this.shouldY = this.y += this.sideLength * h + space * 0.5;
+      this.shouldY = this.y += this.sideLength * sin60 + space * 0.5;
     } else if ((n == 5) || (n == 14) || (n == 18)) {
-      this.shouldY = this.y += this.sideLength * h * 2 + space;
+      this.shouldY = this.y += this.sideLength * sin60 * 2 + space;
     } else if ((n == 15) || (n == 17)) {
-      this.shouldY = this.y += this.sideLength * h * 3 + space * 1.5;
+      this.shouldY = this.y += this.sideLength * sin60 * 3 + space * 1.5;
     } else if (n == 16) {
-      this.shouldY = this.y += this.sideLength * h * 4 + space * 2;
+      this.shouldY = this.y += this.sideLength * sin60 * 4 + space * 2;
     }
   }
 
@@ -71,63 +69,23 @@ function Tile() {
     }
   }
 
-  this.setValue = function(n) {
+  this.setValue = function(n) { // use an array instead.
     this.value = n;
-
-    if (n == 1) { // 2
-      this.setColour("eee4da");
-    } else if (n == 2) { // 4
-      this.setColour("eee1c9");
-    } else if (n == 3) { // 8
-      this.setColour("f3b27a");
-    } else if (n == 4) { // 16
-      this.setColour("f69664");
-    } else if (n == 5) { // 32
-      this.setColour("f77c5f");
-    } else if (n == 6) { // 64
-      this.setColour("f75f3b");
-    } else if (n == 7) { // 128
-      this.setColour("edd073");
-    } else if (n == 8) { // 256
-      this.setColour("edcc62");
-    } else if (n == 9) { // 512
-      this.setColour("edc950");
-    } else if (n == 10) { // 1024
-      this.setColour("edc53f");
-    } else if (n == 11) { // 2048
-      this.setColour("edc22e");
-    } else if (n == 12) { // 4096
-      this.setColour("92df63");
-    } else if (n == 13) { // 8192
-      this.setColour("84e248");
-    } else if (n == 14) { // 16384
-      this.setColour("87e431");
-    } else if (n == 15) { // 32768
-      this.setColour("6be415");
-    } else if (n == 16) { // 65536
-      this.setColour("6b9a58");
-    } else if (n >= 17) { // 131072
-      this.setColour("6b57a1");
-    } else { // includes n <= 0
-      this.red = 205;
-      this.green = 193;
-      this.blue = 180;
-    }
+    this.colour = colours[n];
   }
 
   this.paintDummy = function() {
-    paint_regular_hexagon_unrounded("backHex" + this.id, this.x, this.y, this.sideLength, 205, 193, 180);
+    paint_regular_hexagon(this.x, this.y, this.sideLength, colours[0]);
   }
 
   this.paint = function() {
-    paint_regular_hexagon_unrounded("Hex" + this.id, this.x, this.y, this.sideLength, this.red, this.green, this.blue);
+    paint_regular_hexagon(this.x, this.y, this.sideLength, colours[this.value]);
   }
 
   this.text = function() {
     var num = pow(2, this.value);
-    var length = ceil(log(num) / log(10)); // digits
+    var length = (num + "").length; // digits
 
-    textFont("Calibri");
     textAlign(CENTER, CENTER);
 
     if (length < 3) {
@@ -151,12 +109,5 @@ function Tile() {
       fill(119, 110, 101);
       text(num, this.x, this.y);
     } // else (this.value == 0) -> do nothing
-  }
-
-  this.setColour = function(hexCode) {
-    var bigint = parseInt(hexCode, 16);
-    this.red = (bigint >> 16) & 255;
-    this.green = (bigint >> 8) & 255;
-    this.blue = bigint & 255;
   }
 }
